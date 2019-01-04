882a018fe6319106b66515c1de9aa2e44f5df2d8
Jack Cruden <jackcruden@gmail.com>
2019-01-04 13:55:51 +0000

    Reduce NPM bundle size further

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,26 @@
-const mix = require('laravel-mix');
-var tailwindcss = require('tailwindcss');
+let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+let glob = require('glob-all');
+let PurgecssPlugin = require('purgecss-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+// Analyse JS bundle
+// http://localhost:8888/
+// mix.webpackConfig({
+//     plugins: [
+//         new BundleAnalyzerPlugin(),
+//     ],
+// });
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
@@ -18,3 +28,29 @@ mix.js('resources/js/app.js', 'public/js')
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.js') ],
     });
+
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new PurgecssPlugin({
+
+                // Specify the locations of any files you want to scan for class names.
+                paths: glob.sync([
+                    path.join(__dirname, "resources/views/**/*.blade.php"),
+                    path.join(__dirname, "resources/js/**/*.vue")
+                ]),
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+
+                        // Specify the file extensions to include when scanning for
+                        // class names.
+                        extensions: ["html", "js", "php", "vue"]
+                    }
+                ]
+            })
+        ]
+    });
+
+    mix.version();
+}
