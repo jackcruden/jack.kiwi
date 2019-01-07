45c5fc8e29f733814cc6e0cc14f79cc54b0b66e8
Jack Cruden <jackcruden@gmail.com>
2019-01-07 11:48:16 +0000

    Responsive nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, scale=1.0, user-scalable=no, viewport-fit=cover">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -20,8 +21,8 @@
 </head>
 <body>
     <div id="app">
-        <div class="flex border-b">
-            <div class="p-2 flex-1">
+        <div class="sm:flex border-b">
+            <div class="p-2 pl-3 whitespace-no-wrap">
                 <div class="mb-1">
                     <img src="/images/kiwifruit.svg" class="w-4 align-middle">
                     jack.kiwi
@@ -29,7 +30,7 @@
                 <div class="text-xs text-grey-darker">Jack Cruden's portfolio website.</div>
             </div>
 
-            <nav class="flex justify-end items-center">
+            <nav class="flex-1 text-right w-full justify-end items-center overflow-y-visible">
                 <ul class="x-navigation">
                     <li>
                         <a href="/" class="@active('/')">
@@ -51,20 +52,20 @@
                             Me
                         </a>
                     </li>
-                    <li class="w-full items-center pl-4 pr-2">
-                        <div class="relative">
+                    <li class="w-auto items-center pl-4 pr-4">
+                        <div class="w-full">
                             <ais-index
                                 app-id="ZX7KWEFAEQ"
                                 api-key="0485e1f4466829a42b7c31ee225e7e16"
                                 index-name="posts"
                                 :auto-search="false"
                             >
-                                <ais-input placeholder="Search&hellip;" class="p-2 border rounded-lg text-sm"></ais-input>
+                                <ais-input placeholder="Search&hellip;" class="w-full p-2 border rounded-lg text-sm"></ais-input>
 
                                 <ais-results inline-template>
-                                    <div v-show="searchStore.query.length > 0" class="absolute mt-1 pin-r bg-white rounded-lg border" style="width: 300px;">
+                                    <div v-show="searchStore.query.length > 0" class="absolute w-auto mt-1 pin-r bg-white rounded-lg border mr-3" style="max-width: 300px;">
                                         <ul class="list-reset p-2">
-                                            <li v-for="result in results" class="p-2 border-b">
+                                            <li v-for="result in results" class="p-2 border-b text-left">
                                                 <a :href="'/blog/' + result.slug">
                                                     <ais-highlight :result="result" attribute-name="title"></ais-highlight>
                                                 </a>
@@ -78,7 +79,7 @@
                                     </div>
                                 </ais-results>
                                 <ais-no-results>
-                                    <div class="absolute mt-1 pin-r bg-white rounded-lg border" style="width: 300px;">
+                                    <div class="absolute mt-1 pin-r mr-3 bg-white rounded-lg border w-auto" style="max-width: 300px;">
                                         <ul class="list-reset p-2">
                                             <li class="p-2 border-b">
                                                 No posts found.
