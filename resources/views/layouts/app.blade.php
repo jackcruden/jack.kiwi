6717a9ced42c60143ea429a4de3edca950031a62
Jack Cruden <jackcruden@gmail.com>
2020-12-22 21:46:09 +0000

    Remove turbolinks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -145,6 +145,5 @@
     @yield('footer')
 
     {{--    <script src="{{ mix('/js/app.js') }}" defer></script>--}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/turbolinks/5.2.0/turbolinks.js" integrity="sha512-G3jAqT2eM4MMkLMyQR5YBhvN5/Da3IG6kqgYqU9zlIH4+2a+GuMdLb5Kpxy6ItMdCfgaKlo2XFhI0dHtMJjoRw==" crossorigin="anonymous"></script>
 </body>
 </html>
